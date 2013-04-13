be4736978932993e741629de8224459fc64abf88
Seth Schwartzman <seth@newrelic.com>
2013-04-13 11:41:21 -0400

    Prettyed-up code a bit. Moved example server-status files to "samples"

--- newrelic_apache_httpd_extension.rb
+++ newrelic_apache_httpd_extension.rb
@@ -9,22 +9,22 @@ require "net/http"
 # The entire agent should be enclosed in a "ApacheHTTPDAgent" module
 #
 module ApacheHTTPDAgent
-
   #
   # Agent, Metric and PollCycle classes
   #
-  # Each agent module must have an Agent, Metric and PollCycle class that inherits from their
+  # Each agent module must have an Agent, Metric and PollCycle class that
+  # inherits from their
   # Component counterparts as you can see below.
   #
   class Agent < NewRelic::Plugin::Agent::Base
-	
-   		
-	agent_config_options :hostname, :username, :password, :port, :agent_name, :debug
+
+    agent_config_options :hostname, :username, :password, :port, :agent_name, :debug
     agent_guid "com.newrelic.examples.apache.httpd"
     agent_version "0.0.1"
-	    
+
     #
-    # Each agent class must also include agent_human_labels. agent_human_labels requires:
+    # Each agent class must also include agent_human_labels. agent_human_labels
+    # requires:
     # A friendly name of your component to appear in graphs.
     # A block that returns a friendly name for this instance of your component.
 
@@ -34,88 +34,101 @@ module ApacheHTTPDAgent
 
     def setup_metrics
       @@APACHE_STAT_URL = URI.parse("http://#{hostname}/server-status?auto")
-  		@@metric_types = Hash.new("ms")  
-  	  @@metric_types["Total Accesses"] = "accesses"
-  		@@metric_types["Total kBytes"] = "kb"
-  		@@metric_types["CPULoad"] = "%"
-  		@@metric_types["Uptime"] = "sec"
-  		@@metric_types["ReqPerSec"] = "requests"
-  		@@metric_types["BytesPerSec"] = "bytes/sec"
-  		@@metric_types["BytesPerReq"] = "bytes/req"
-  		@@metric_types["BusyWorkers"] = "workers"
-  		@@metric_types["IdleWorkers"] = "workers"
-  		@@metric_types["ConnsTotal"] = "connections"
-  		@@metric_types["ConnsAsyncWriting"] = "connections"
-  		@@metric_types["ConnsAsyncKeepAlive"] = "connections"
-  		@@metric_types["ConnsAsyncClosing"] = "connections"
-  		@@metric_types["Scoreboard/WaitingForConnection"] = "workers"
-  		@@metric_types["Scoreboard/StartingUp"] = "workers"
-  		@@metric_types["Scoreboard/ReadingRequest"] = "workers"
-  		@@metric_types["Scoreboard/SendingReply"] = "workers"
-  		@@metric_types["Scoreboard/KeepAliveRead"] = "workers"
-  		@@metric_types["Scoreboard/DNSLookup"] = "workers"
-  		@@metric_types["Scoreboard/ClosingConnection"] = "workers"
-  		@@metric_types["Scoreboard/Logging"] = "workers"
-  		@@metric_types["Scoreboard/GracefullyFinishing"] = "workers"
-  		@@metric_types["Scoreboard/IdleCleanupOfWorker"] = "workers"
-  		@@metric_types["Scoreboard/OpenSlotWithNoCurrentProcess"] = "workers"
-   		@@scoreboard_values = Hash["_", "WaitingForConnection", "S", "StartingUp", "R", "ReadingRequest", "W", "SendingReply", "K", "KeepAliveRead", "D", "DNSLookup", "C", "ClosingConnection", "L", "Logging", "G", "GracefullyFinishing", "I", "IdleCleanupOfWorker", ".", "OpenSlotWithNoCurrentProcess"]
+      
+      @@metric_types = Hash.new("ms")
+      @@metric_types["Total Accesses"] = "accesses"
+      @@metric_types["Total kBytes"] = "kb"
+      @@metric_types["CPULoad"] = "%"
+      @@metric_types["Uptime"] = "sec"
+      @@metric_types["ReqPerSec"] = "requests"
+      @@metric_types["BytesPerSec"] = "bytes/sec"
+      @@metric_types["BytesPerReq"] = "bytes/req"
+      @@metric_types["BusyWorkers"] = "workers"
+      @@metric_types["IdleWorkers"] = "workers"
+      @@metric_types["ConnsTotal"] = "connections"
+      @@metric_types["ConnsAsyncWriting"] = "connections"
+      @@metric_types["ConnsAsyncKeepAlive"] = "connections"
+      @@metric_types["ConnsAsyncClosing"] = "connections"
+      @@metric_types["Scoreboard/WaitingForConnection"] = "workers"
+      @@metric_types["Scoreboard/StartingUp"] = "workers"
+      @@metric_types["Scoreboard/ReadingRequest"] = "workers"
+      @@metric_types["Scoreboard/SendingReply"] = "workers"
+      @@metric_types["Scoreboard/KeepAliveRead"] = "workers"
+      @@metric_types["Scoreboard/DNSLookup"] = "workers"
+      @@metric_types["Scoreboard/ClosingConnection"] = "workers"
+      @@metric_types["Scoreboard/Logging"] = "workers"
+      @@metric_types["Scoreboard/GracefullyFinishing"] = "workers"
+      @@metric_types["Scoreboard/IdleCleanupOfWorker"] = "workers"
+      @@metric_types["Scoreboard/OpenSlotWithNoCurrentProcess"] = "workers"
+      
+      @@scoreboard_values = Hash.new("NotDefined")
+      @@scoreboard_values["_"] = "WaitingForConnection"
+      @@scoreboard_values["S"] = "StartingUp"
+      @@scoreboard_values["R"] = "ReadingRequest"
+      @@scoreboard_values["W"] = "SendingReply"
+      @@scoreboard_values["K"] = "KeepAliveRead"
+      @@scoreboard_values["D"] = "DNSLookup"
+      @@scoreboard_values["C"] = "ClosingConnection"
+      @@scoreboard_values["L"] = "Logging"
+      @@scoreboard_values["G"] = "GracefullyFinishing"
+      @@scoreboard_values["I"] = "IdleCleanupOfWorker"
+      @@scoreboard_values["."] = "OpenSlotWithNoCurrentProcess"
     end
 
     def poll_cycle
       stats = apache_httpd_stats()
-      stats.each_key { |mtree| 
-      	mout = "HTTPD/#{hostname}"
-      	if "#{mtree}".start_with?("Scoreboard")
-      		mout = "#{mout}/#{mtree}"
-      	elsif @@metric_types[mtree] == "workers"
-      		mout = "#{mout}/Workers/#{mtree}"
-      	elsif @@metric_types[mtree] == "connections"
-      		mout = "#{mout}/Connections/#{mtree}"
-      	elsif @@metric_types[mtree] == "%"
-      		stats[mtree] = 100 * stats[mtree].to_f
-      		mout = "#{mout}/#{mtree}"
-      	else 
-      		mout = "#{mout}/#{mtree}"
-      	end
-      	if "#{debug}" == "true"
-          puts("#{mout}[#{@@metric_types[mtree]}]: #{stats[mtree]}")  
-      	else
-      	  report_metric "#{mout}", @@metric_types[mtree], stats[mtree]      	  
-      	end
+      stats.each_key { |mtree|
+        mout = "HTTPD/#{hostname}"
+        if "#{mtree}".start_with?("Scoreboard")
+          mout = "#{mout}/#{mtree}"
+        elsif @@metric_types[mtree] == "workers"
+          mout = "#{mout}/Workers/#{mtree}"
+        elsif @@metric_types[mtree] == "connections"
+          mout = "#{mout}/Connections/#{mtree}"
+        elsif @@metric_types[mtree] == "%"
+          stats[mtree] = 100 * stats[mtree].to_f
+          mout = "#{mout}/#{mtree}"
+        else
+          mout = "#{mout}/#{mtree}"
+        end
+        if "#{debug}" == "true"
+          puts("#{mout}[#{@@metric_types[mtree]}]: #{stats[mtree]}")
+        else
+          report_metric "#{mout}", @@metric_types[mtree], stats[mtree]
+        end
       }
     rescue => e
       $stderr.puts "#{e}: #{e.backtrace.join("\n  ")}"
     end
 
     private
-    
-    def apache_httpd_stats	
-	    begin
-	    	resp = ::Net::HTTP.get_response(@@APACHE_STAT_URL)
-	    rescue => e
-	      $stderr.puts "#{e}: #{e.backtrace.join("\n  ")}"
-	    end
-      	data = resp.body
-      	
-      	lines = data.split("\n")
-      	stats = Hash.new
-        lines.each { |line| 
-        	marray = line.split(": ")
-        	if marray[0] == "Scoreboard"
-        		@@scoreboard_values.each { |sk, sv| 
-    					mcount = marray[1].count sk
-           		sn = "#{marray[0]}/#{sv}"
-    					stats[sn] = mcount 
-    				}
-        	else
-        		stats["#{marray[0]}"] = marray[1]
-        	end
-        }
-		  return stats
-    end  
+
+    def apache_httpd_stats
+      begin
+        resp = ::Net::HTTP.get_response(@@APACHE_STAT_URL)
+      rescue => e
+        $stderr.puts "#{e}: #{e.backtrace.join("\n  ")}"
+      end
+      data = resp.body
+
+      lines = data.split("\n")
+      stats = Hash.new
+      lines.each { |line|
+        marray = line.split(": ")
+        if marray[0] == "Scoreboard"
+          @@scoreboard_values.each { |sk, sv|
+            mcount = marray[1].count sk
+            sn = "#{marray[0]}/#{sv}"
+            stats[sn] = mcount
+          }
+        else
+          stats["#{marray[0]}"] = marray[1]
+        end
+      }
+      return stats
+    end
   end
-  
+
   NewRelic::Plugin::Setup.install_agent :apachehttpd, self
 
   # Launch the agent; this never returns.
